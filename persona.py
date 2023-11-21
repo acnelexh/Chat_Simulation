6e9862b21a736f59b060807d4c66a039c6ff3361
mengyuanwu3209 <mw3209@columbia.edu>
2023-11-21 11:51:00 -0500

    Emotion and persona generator done

--- persona.py
+++ persona.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import enum
 import random
+import itertools
 
 class PersonaType(enum.Enum):
     CHATBOT = 1
@@ -14,17 +15,17 @@ class PersonaType(enum.Enum):
 class PersonaGenerator:
     def __init__(self):
         self.gender_categories = ['Male', 'Female', 'Non-binary/Gender fluid', 'Other']
-        self.cultural_background_categories = ['North American', 'Latin American', 'European', 'African', 'Asian', 'Middle Eastern', 'Australian', 'Other']
-        self.occupation_categories = ['Student', 'Healthcare', 'Technology', 'Education', 'Arts and Entertainment', 'Business', 'Manual Labor', 'Unemployed', 'Retired', 'Other']
-        self.education_categories = ['No Formal Education', 'Primary School', 'Secondary School', 'Vocational Training', 'Associate Degree', 'Bachelor\'s Degree', 'Master\'s Degree', 'Doctorate', 'Other']
-        self.family_dynamics_categories = ['Single Parent', 'Nuclear Family', 'Extended Family', 'Childless Couple', 'Single', 'Widowed', 'Divorced', 'Living with Friends', 'Other']
-        self.relationship_status_categories = ['Single', 'In a Relationship', 'Engaged', 'Married', 'Divorced', 'Widowed', 'Separated', 'It\'s Complicated', 'Other']
+        self.cultural_background_categories = ['North American', 'Latin American', 'European', 'African', 'Asian', 'Middle Eastern', 'Australian']
+        self.occupation_categories = ['Student', 'Healthcare', 'Technology', 'Education', 'Arts and Entertainment', 'Business', 'Manual Labor', 'Unemployed', 'Retired']
+        self.education_categories = ['No Formal Education', 'Primary School', 'Secondary School', 'Vocational Training', 'Associate Degree', 'Bachelor\'s Degree', 'Master\'s Degree', 'Doctorate']
+        self.family_dynamics_categories = ['Single Parent', 'Nuclear Family', 'Extended Family', 'Childless Couple', 'Single', 'Widowed', 'Divorced', 'Living with Friends']
+        self.relationship_status_categories = ['Single', 'In a Relationship', 'Engaged', 'Married', 'Divorced', 'Widowed', 'Separated', 'It\'s Complicated']
         self.attachment_styles = ['Secure', 'Anxious', 'Avoidant', 'Fearful']
-        self.stress_triggers_categories = ['Work', 'Finance', 'Family', 'Health', 'Relationships', 'Time Management', 'Personal Growth', 'Social Interactions', 'Other']
-        self.coping_strategies_categories = ['Exercise', 'Meditation', 'Talking to Friends/Family', 'Hobbies', 'Professional Help', 'Substance Use', 'Avoidance', 'Problem-Solving', 'Other']
-        self.significant_past_events_categories = ['Loss of a Loved One', 'Major Illness', 'Moving to a New Place', 'Career Change', 'Marriage', 'Divorce', 'Birth of a Child', 'Traumatic Event', 'Other']
-        self.likes_dislikes_categories = ['Sports', 'Arts', 'Technology', 'Nature', 'Reading', 'Music', 'Cooking', 'Travel', 'Other']
-        self.goals_passions_aspirations_categories = ['Career Development', 'Personal and Intellectual Growth', 'Health and Well-being', 'Relationships and Family', 'Financial Goals', 'Exploration and Adventure', 'Creative and Artistic Pursuits', 'Helping Others and Social Impact', 'Environmental Engagement', 'Personal Fulfillment', 'Other']
+        self.stress_triggers_categories = ['Work', 'Finance', 'Family', 'Health', 'Relationships', 'Time Management', 'Personal Growth', 'Social Interactions']
+        self.coping_strategies_categories = ['Exercise', 'Meditation', 'Talking to Friends/Family', 'Hobbies', 'Professional Help', 'Substance Use', 'Avoidance', 'Problem-Solving']
+        self.significant_past_events_categories = ['Loss of a Loved One', 'Major Illness', 'Moving to a New Place', 'Career Change', 'Marriage', 'Divorce', 'Birth of a Child', 'Traumatic Event']
+        self.likes_dislikes_categories = ['Sports', 'Arts', 'Technology', 'Nature', 'Reading', 'Music', 'Cooking', 'Travel']
+        self.goals_passions_aspirations_categories = ['Career Development', 'Personal and Intellectual Growth', 'Health and Well-being', 'Relationships and Family', 'Financial Goals', 'Exploration and Adventure', 'Creative and Artistic Pursuits', 'Helping Others and Social Impact', 'Environmental Engagement', 'Personal Fulfillment']
         self.mbti = ['ISTJ', 'ISFJ', 'INFJ', 'INTJ', 'ISTP', 'ISFP', 'INFP', 'INTP', 'ESTP', 'ESFP', 'ENFP', 'ENTP', 'ESTJ', 'ESFJ', 'ENFJ', 'ENTJ']
 
     def generate_gender(self):
@@ -32,7 +33,7 @@ class PersonaGenerator:
 
     def generate_cultural_background(self):
         # Simplified weights based on estimated global population distribution
-        weights = [15, 8, 10, 15, 30, 8, 4, 10]
+        weights = [15, 8, 10, 15, 30, 8, 4]
         return random.choices(self.cultural_background_categories, weights=weights, k=1)[0]
 
     def generate_mbti(self):
@@ -47,26 +48,33 @@ class PersonaGenerator:
 
     def generate_occupation(self):
         # Rough estimate of global occupational distribution, World Employment and Social Outlook report by the International Labour Organization (ILO) 
-        weights = [15, 10, 15, 10, 5, 10, 15, 5, 5, 10]  # Percentages for Student, Healthcare, Technology, Education, Arts and Entertainment, Business, Manual Labor, Unemployed, Retired, Other
+        weights = [10, 8, 8, 8, 5, 15, 30, 10, 6]  # Percentages for Student, Healthcare, Technology, Education, Arts and Entertainment, Business and Finance, Manual Labor, Unemployed, Retired] #Based on BLS
         return random.choices(self.occupation_categories, weights=weights, k=1)[0]
 
     def generate_education(self):
         # Rough estimate of global education level distribution, 
-        weights = [10, 20, 25, 5, 5, 15, 10, 5, 5]  # Percentages for No Formal Education, Primary School, Secondary School, Vocational Training, Associate Degree, Bachelor's Degree, Master's Degree, Doctorate, Other
+        weights = [10, 20, 25, 5, 5, 20, 10, 5]  # Percentages for No Formal Education, Primary School, Secondary School, Vocational Training, Associate Degree, Bachelor's Degree, Master's Degree, Doctorate
         return random.choices(self.education_categories, weights=weights, k=1)[0]
 
     def generate_family_dynamics(self):
         # Rough estimate of global family dynamics
-        weights = [10, 25, 20, 10, 10, 5, 10, 5, 5]  # Percentages for Single Parent, Nuclear Family, Extended Family, Childless Couple, Single, Widowed, Divorced, Living with Friends, Other
+        weights = [10, 30, 20, 10, 10, 5, 10, 5]  # Percentages for Single Parent, Nuclear Family, Extended Family, Childless Couple, Single, Widowed, Divorced, Living with Friends
         return random.choices(self.family_dynamics_categories, weights=weights, k=1)[0]
 
     def generate_relationship_status(self):
         # Rough estimate of global relationship status
-        weights = [25, 20, 5, 25, 5, 5, 5, 5, 5]  # Percentages for Single, In a Relationship, Engaged, Married, Divorced, Widowed, Separated, It's Complicated, Other
+        weights = [30, 20, 5, 25, 5, 5, 5, 5]  # Percentages for Single, In a Relationship, Engaged, Married, Divorced, Widowed, Separated, It's Complicated
         return random.choices(self.relationship_status_categories, weights=weights, k=1)[0]
 
     def generate_persona(self):
-        persona = PersonaGenerator(
+        # Generate likes
+        likes = random.sample(self.likes_dislikes_categories, k=2)
+        # Filter out the likes to get remaining categories
+        remaining_categories = [category for category in self.likes_dislikes_categories if category not in likes]
+        # Generate dislikes from the remaining categories
+        dislikes = random.sample(remaining_categories, k=2)
+
+        persona = Persona(
             age=random.randint(2, 80), # when can babies start using computers to estimated age of not being able to use computers
             gender=self.generate_gender(), #based on global population
             cultural_background=self.generate_cultural_background(), #based on global population
@@ -82,47 +90,51 @@ class PersonaGenerator:
             emotional_range=random.randint(1, 5),
             stress_triggers=random.sample(self.stress_triggers_categories, k=3),
             coping_strategies=random.sample(self.coping_strategies_categories, k=3),
-            significant_events=random.sample(self.significant_past_events_categories, k=3),
-            likes_dislikes=random.sample(self.likes_dislikes_categories, k=3),
+            significant_events=random.sample(self.significant_past_events_categories, k=1),
+            likes=likes,
+            dislikes = dislikes,
             goals_aspirations=random.sample(self.goals_passions_aspirations_categories, k=3),
             physical_health=random.randint(1, 5),
             mental_health=random.randint(1, 5)
         )
         return persona
 
+class Persona:
+    def __init__(self, age, gender, cultural_background, occupation, education, family_dynamics, relationship_status, mbti, attachment_style, emotional_intelligence, typical_mood, emotional_range, stress_triggers, coping_strategies, significant_events, likes, dislikes, goals_aspirations, physical_health, mental_health):
+        self.age = age
+        self.gender = gender
+        self.cultural_background = cultural_background
+        self.occupation = occupation
+        self.education = education
+        self.family_dynamics = family_dynamics
+        self.relationship_status = relationship_status
+        self.mbti = mbti
+        self.attachment_style = attachment_style
+        self.emotional_intelligence = emotional_intelligence
+        self.typical_mood = typical_mood
+        self.emotional_range = emotional_range
+        self.stress_triggers = stress_triggers
+        self.coping_strategies = coping_strategies
+        self.significant_events = significant_events
+        self.likes = likes
+        self.dislikes = dislikes
+        self.goals_aspirations = goals_aspirations
+        self.physical_health = physical_health
+        self.mental_health = mental_health
 
-class Persona():
-    def __init__(self, p_type: PersonaType, age=None, gender=None, cultural_background=None, occupation=None, education=None, family_dynamics=None, relationship_status=None, mbti=None, attachment_style=None, emotional_intelligence=None, typical_mood=None, emotional_range=None, stress_triggers=None, coping_strategies=None, significant_events=None, likes_dislikes=None, goals_aspirations=None, physical_health=None, mental_health=None):
-        if p_type == PersonaType.CHATBOT:
-            self.persona = self._define_chatbot()
-        else:
+    def __str__(self):
+        # This method is for printing the persona's attributes in a readable format.
+        return f"Persona(age={self.age}, gender='{self.gender}', cultural_background='{self.cultural_background}', occupation='{self.occupation}', education='{self.education}', family_dynamics='{self.family_dynamics}', relationship_status='{self.relationship_status}', mbti='{self.mbti}', attachment_style='{self.attachment_style}', emotional_intelligence={self.emotional_intelligence}, typical_mood={self.typical_mood}, emotional_range={self.emotional_range}, stress_triggers={self.stress_triggers}, coping_strategies={self.coping_strategies}, significant_events={self.significant_events}, likes={self.likes}, dislikes={self.dislikes}, goals_aspirations={self.goals_aspirations}, physical_health={self.physical_health}, mental_health={self.mental_health})"
+
+
+        # if p_type == PersonaType.CHATBOT:
+        #     self.persona = self._define_chatbot()
             # Store the attributes for a user persona
-            self.age = age
-            self.gender = gender
-            self.cultural_background = cultural_background
-            self.occupation = occupation
-            self.education = education
-            self.family_dynamics = family_dynamics
-            self.relationship_status = relationship_status
-            self.mbti = mbti
-            self.attachment_style = attachment_style
-            self.emotional_intelligence = emotional_intelligence
-            self.typical_mood = typical_mood
-            self.emotional_range = emotional_range
-            self.stress_triggers = stress_triggers
-            self.coping_strategies = coping_strategies
-            self.significant_events = significant_events
-            self.likes_dislikes = likes_dislikes
-            self.goals_aspirations = goals_aspirations
-            self.physical_health = physical_health
-            self.mental_health = mental_health
-
-            self.persona = self._define_user()
-
     def _define_chatbot(self):
         '''
         Generate persona for the chatbot
         '''
+
         pass
     
     def _define_user(self):
@@ -141,56 +153,36 @@ class Persona():
         return self.persona
     
     def generate_emotions_and_arousals():
-        # Ekman's 6 emotions
-        emotions = ["Surprise", "Happy", "Neural", "Sad", "Fear", "Anger", "Disgust"]
+        # Ekman's 6 emotions + neutral
+        #neutral_emotions = ["Neural"]
+        positive_emotions = ["Surprise", "Happy"]
+        negative_emotions = ["Sad", "Fear", "Anger", "Disgust"]
 
-        # arousal levels
-        arousal_levels = ["weak", "medium", "strong"]
+        # Generate combinations: Positive to Negative
+        positive_to_negative = [(pe, ne) for pe in positive_emotions for ne in negative_emotions]
 
-        # Randomly choose first emotion
-        emotion1 = random.choice(emotions)
+        # Generate combinations: Negative to Positive
+        negative_to_positive = [(ne, pe) for pe in positive_emotions for ne in negative_emotions]
 
-        # Remove emotions within 2 positions of the first emotion
-        emotion_index = emotions.index(emotion1)
-        forbidden_indices = [(emotion_index - 1) % 6, emotion_index, (emotion_index + 1) % 6]
-        allowed_emotions = [emotions[i] for i in range(6) if i not in forbidden_indices]
+        # Combine both lists
+        drastic_changes = positive_to_negative + negative_to_positive
 
-        # Randomly choose second emotion from allowed emotions
-        emotion2 = random.choice(allowed_emotions)
+        # Assuming you have your drastic_changes list ready
+        arousal_levels = ['weak', 'medium', 'strong']
 
-        # Randomly choose arousal levels for both emotions
-        emotion1_arousal = random.choice(arousal_levels)
-        emotion2_arousal = random.choice(arousal_levels)
+        # Generate combinations of each emotion with an arousal level
+        emotion_arousal_combinations = list(itertools.product(arousal_levels, repeat=2))
 
-        return emotion1, emotion2, emotion1_arousal, emotion2_arousal
+        # Now pair each emotion change with each arousal level combination
+        all_combinations = list(itertools.product(drastic_changes, emotion_arousal_combinations))
+
+        # for combo in all_combinations:
+        #     emotion_pair, arousal_pair = combo
+        #     print(f"Emotions: {emotion_pair}, Arousals: {arousal_pair}")
+
+        return all_combinations
     
 
-generator = PersonaGenerator()
-random_persona = generator.generate_persona()
-# Step 3: Output the generated persona
-print(random_persona)
-
-
-
-        # persona1 = Persona{
-        #     age=30, # when can babies start using computers to estimated age of not being able to use computers
-        #     gender= 'female', #based on global population
-        #     cultural_background= 'European', #based on global population
-        #     occupation= 'Healthcare',  # Based on rough estimate of global occupational distribution
-        #     education= 'Doctorate',  # Based on rough estimate of global education level distribution
-        #     family_dynamics= 'Single Parent',  # Based on rough estimate of global family dynamics
-        #     relationship_status = 'Single',  # Based on rough estimate of global relationship status
-        #     mbti='ENTJ', #based on global population
-        #     attachment_style = 'Avoidant',
-
-        #     emotional_intelligence=3,
-        #     typical_mood=2,
-        #     emotional_range=4,
-        #     stress_triggers = ['Work', 'Finance', 'Family'],
-        #     coping_strategies = ['Exercise', 'Meditation', 'Talking to Friends/Family'],
-        #     significant_events = ['Marriage'], 
-        #     likes_dislikes = ['Technology', 'Nature', 'Reading'],
-        #     goals_aspirations = ['Career Development', 'Health and Well-being', 'Relationships and Family'],
-        #     physical_health= 3,
-        #     mental_health= 5
-        # }
+    # generator = PersonaGenerator()
+    # random_persona = generator.generate_persona()
+    # print(random_persona)
